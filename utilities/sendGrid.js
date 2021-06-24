3f0a04c99419a60662e4ddf249f6d6da24035392
cybersagittarius <cybersagittarius@gmail.com>
2021-06-24 10:39:23 +0200

    updated going out message context in the template

--- utilities/sendGrid.js
+++ utilities/sendGrid.js
@@ -7,13 +7,20 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendPwResetEmail = (token) => {
 
 const msg = {
-  to: 'service.exchange.platform.2021@hotmail.com', // Change to your recipient
+  //johnnycbcs@gmail.com, you can replace this with your own email address to try
+  to: 'cybersagittarius@gmail.com', // Change to your recipient
+  //IMPORTANT!!!!!! The following email shall not be changed !!!!!!!!!!!!!
   from: 'service.exchange.platform.2021@gmail.com', // Change to your verified sender
   subject: 'Here comes the reset password link!!',
   text: `There is link to reset your password`,
   html: `
         <strong>and easy to do anywhere, even with Node.js</strong>
         <a href="http://localhost:3000/reset_password/${token}"> Click Here to Change Password </a>
+        <ol>
+          <li>The token is valid for 60 minutes by the time the email sent to you.</li>
+          <li>The link can only be used once.</li>
+          <li>If you follow the link but without resetting a new password, please repeat the same steps to go back to the 'forget password' page to request the service.</li>
+        </ol>
         `,
 }
 // sgMail
